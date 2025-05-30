92a39e1e70c741d5a1228e3f2e1abf4c10accf93
Divyanshu <divyanshu@Divyanshus-MacBook-Air.local>
2025-05-30 18:07:55 +0530

    created authMiddleware, and done with the user/upadateInfo route

---
+++ backend/middleware.js
@@ -0,0 +1,26 @@
+import { JWT_SECRET } from './config';
+
+const jwt = require('jsonwebtoken')
+
+const authMiddleWare = (req,res,next)=>{
+    //authenticate the user
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(403).json({})
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+         const decoded = jwt.verify(token[0],JWT_SECRET);
+         req.userId = decoded.id;
+         next();
+    } catch (error) {
+       return res.status(403).json({
+            message:"Un-authorized Access!"
+        })
+    }
+
+}
+
+module.exports = {
+    authMiddleWare 
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { userSchemaSignIn,userSchemaSignUp } = require('../zodSchemas');
 const { User } = require('../UserModel');
-const {jwt}  = require('jsonwebtoken')
-const {JWT_SECRET} = require('../config')
+const jwt  = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config');
+const { authMiddleWare } = require('../middleware');
 const UserRouter = express.Router()
 
 
@@ -21,9 +22,11 @@ UserRouter.post('/signup',async (req,res)=>{
             message:'user already exist!'
         })
     }
+    // hash the password 
+    const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({
         email,
-        password,
+        password:hashedPassword,
         lastname,
         firstname
     })
@@ -45,18 +48,37 @@ UserRouter.post('/signin', async (req,res)=>{
     // database check for user existence.
     const userExist = await User.findOne({email:email});
     if(!userExist){
-        return res.status(403).json({
+        return res.status(411).json({
             message:"User hasn't registered yet!"
+        })
+    }
+    //password matching check
+    const matched = await bcrypt.compare(password, userExist.password);
+    if(!matched){
+        return res.status(411).json({
+            message:"user email/password incorrect!"
         })
     }
     // creating a jsonwebtoken 
     const token = jwt.sign({ id:userExist._id }, JWT_SECRET);
     res.status(200).json({
-        userId : token
+        userId : `Bearer ${token}`
     })
 })
 
-UserRouter.post('/updateInfo',(req,res)=>{
+UserRouter.post('/updateInfo',authMiddleWare,async (req,res)=>{
+    const body = req.body;
+    const userId = req.userId;
+    try {
+        const updatedUserInfo = await User.findByIdAndUpdate(userId, body, {new:true});
+        return res.status(200).status({
+            message:'Information Updated Successfully!'
+        })
+    } catch (error) {
+        res.status(500).json({
+            message:"Something happened in while doing DB query!"
+        })
+    }
     
 })
 
